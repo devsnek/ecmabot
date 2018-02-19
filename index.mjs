610e39b80937c47b65eda8beeea732061feacce0
Gus Caplan <me@gus.host>
2018-02-19 11:43:05 -0600

    move to hastebin

--- index.mjs
+++ index.mjs
@@ -1,7 +1,6 @@
 import Discord from 'discord.js';
 import evil from './evil';
 import config from './config';
-import gist from 'snekgist';
 import request from 'snekfetch';
 
 const client = new Discord.Client();
@@ -10,12 +9,10 @@ const cblockre = /(^```js)|(```$)/g;
 
 async function respond(message, content) {
   if (content.length > 1985) {
-    const { html_url } = await gist({ 'out.js': { content } });
-    const location = await request
-      .post('https://git.io')
-      .attach('url', html_url)
-      .then((r) => r.headers.location);
-    return message.reply(`**Output was too long and was uploaded to ${location}**`);
+    const key = await request.post('https://hastebin.com/documents')
+      .send(content)
+      .then((r) => r.body.key);
+    return message.reply(`**Output was too long and was uploaded to https://hastebin.com/${key}.js**`);
   } else {
     return message.reply(content, { code: 'js' });
   }
