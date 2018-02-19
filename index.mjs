634c4f6f47dcede348ee592963591183bf36f9ae
Gus Caplan <me@gus.host>
2018-02-18 23:30:39 -0600

    gist

--- index.mjs
+++ index.mjs
@@ -1,10 +1,25 @@
 import Discord from 'discord.js';
 import evil from './evil';
 import config from './config';
+import gist from 'snekgist';
+import request from 'snekfetch';
 
 const client = new Discord.Client();
 
-const cblockre = /^```js|```$/;
+const cblockre = /(^```js)|(```$)/g;
+
+async function send(channel, content) {
+  if (content.length > 1985) {
+    const { html_url } = await gist({ 'out.js': { content } });
+    const location = await request
+      .post('https://git.io')
+      .attach('url', html_url)
+      .then((r) => r.headers.location);
+    return channel.send(`**Output was too long and was uploaded to ${location}**`);
+  } else {
+    return channel.send(content, { code: 'js' });
+  }
+}
 
 client.on('message', async (message) => {
   const prefix = client.user.toString();
@@ -13,10 +28,10 @@ client.on('message', async (message) => {
   let content = message.content.replace(client.user.toString(), '').trim();
 
   if (cblockre.test(content))
-    content = content.replace(cblockre, '');
+    content = content.replace(cblockre, '').trim();
 
   const out = await evil(content);
-  message.channel.send(out, { code: 'js' });
+  send(message.channel, out);
 });
 
 client.on('error', (err) => {
